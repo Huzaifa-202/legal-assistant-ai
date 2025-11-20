54ea57a3e3731ad6c803a06704e3d8c3e000acf9
Huzaifa Asad <huzaifaasad4838@gmail.com>
2025-11-20 21:38:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import asyncio
 from app import create_app
 
-# Gunicorn with aiohttp will call this async function
-# Do NOT use asyncio.run() here
-app = create_app
+# For Gunicorn with aiohttp
+app = asyncio.run(create_app())
 
 
 
